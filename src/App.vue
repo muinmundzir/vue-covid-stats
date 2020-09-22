cb89b29d20f8c559eb1452fe78fa602f923a5253
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2020-09-22 10:03:54 +0700

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-navbar></app-navbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/shared/Navbar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    appNavbar: Navbar,
+  },
+};
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Nunito", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/shared/Navbar.vue
@@ -0,0 +1,80 @@
+<template>
+  <nav>
+    <div class="logo">
+      <img alt="Vue logo" src="../../assets/logo.png" />
+      <h2>Covid19</h2>
+    </div>
+    <ul class="nav">
+      <li class="nav-item">
+        <a href="#" class="nav-link active">Home</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">News</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">About</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+nav {
+  background-color: #323736;
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+}
+
+.logo {
+  padding: 10px;
+  display: flex;
+  align-items: center;
+}
+
+.logo img {
+  height: 30px;
+}
+
+.logo h2 {
+  color: white;
+  margin-left: 5px;
+  font-size: 20px;
+  text-transform: uppercase;
+}
+
+.nav {
+  display: flex;
+  align-items: center;
+  list-style-type: none;
+}
+
+.nav-item {
+  margin-right: 20px;
+  height: 100%;
+}
+
+.nav-link {
+  padding: 20px 10px;
+  display: inline-block;
+  height: 100%;
+  color: white;
+  font-size: 16px;
+  text-transform: uppercase;
+  text-decoration: none;
+}
+
+.nav-link:hover {
+  background-color: #3a3f3e;
+  font-size: 17px;
+}
+
+.nav-link.active {
+  font-size: 17px;
+  font-weight: bold;
+}
+</style>
